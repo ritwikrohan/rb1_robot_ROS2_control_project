3ea1bccbd66e5b2f937076169ebe5a8d6b852541
Ritwik Rohan <ritwikrohan7@gmail.com>
2023-12-07 18:48:50 +0000

    Differential drive controller added to RB1

---
+++ rb1_ros2_description/launch/rb1_controllers.launch.py
@@ -0,0 +1,40 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    load_joint_state_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
+            'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    load_diff_drive_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
+            'diff_drive_controller'],
+        output='screen'
+    )
+
+    load_elevator_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
+            'elevator_effort_controller'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        load_joint_state_controller,
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_controller,
+                on_exit=[load_diff_drive_controller],
+            )
+        ),
+    ])
--- rb1_ros2_description/launch/rb1_ros2_xacro.launch.py
+++ rb1_ros2_description/launch/rb1_ros2_xacro.launch.py
@@ -1,13 +1,15 @@
 import os
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.substitutions import Command, LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from ament_index_python.packages import get_package_prefix
 from launch_ros.descriptions import ParameterValue
-
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+import xacro
 
 def generate_launch_description():
 
@@ -32,7 +34,7 @@ def generate_launch_description():
     print("GAZEBO MODELS PATH=="+str(os.environ["GAZEBO_MODEL_PATH"]))
     print("GAZEBO PLUGINS PATH=="+str(os.environ["GAZEBO_PLUGIN_PATH"]))
 
-    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='True')
 
     # Define the launch arguments for the Gazebo launch file
     gazebo_launch_args = {
@@ -58,9 +60,13 @@ def generate_launch_description():
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
-        namespace=robot_name_1,
-        parameters=[{'frame_prefix': robot_name_1+'/', 'use_sim_time': use_sim_time,
-                     'robot_description': ParameterValue(Command(['xacro ', robot_desc_path, ' robot_name:=', robot_name_1]), value_type=str)}],
+        #namespace=robot_name_1,
+        parameters=[{
+
+                      #'frame_prefix': robot_name_1+'/',
+                       'use_sim_time': use_sim_time,
+                      'robot_description': ParameterValue(Command(['xacro ', robot_desc_path,
+                                                                 ' robot_name:=', robot_name_1]), value_type=str)}],
         output="screen"
     )
 
@@ -68,11 +74,52 @@ def generate_launch_description():
         package='gazebo_ros',
         executable='spawn_entity.py',
         arguments=['-entity', robot_name_1, '-x', '0.0', '-y', '0.0', '-z', '0.0',
-                   '-topic', robot_name_1+'/robot_description']
+                   '-topic' ,'/robot_description']
+    )
+
+    load_joint_state_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    load_diff_drive_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
+            'diff_drive_controller'],
+        output='screen'
+    )
+
+    load_elevator_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
+             'elevator_effort_controller'],
+        output='screen'
     )
 
     return LaunchDescription([
         gazebo,
         rsp_robot1,
         spawn_robot1,
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_robot1,
+                  on_exit=[
+                    TimerAction(
+                        period=100.0,
+                        actions=[load_joint_state_controller],
+                    )
+                ],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_controller,
+                on_exit=[load_diff_drive_controller],
+            )
+        )
+        # ,RegisterEventHandler(
+        #     event_handler=OnProcessExit(
+        #         target_action=load_diff_drive_controller,
+        #         on_exit=[load_elevator_controller],
+        #     )
+        # )
     ])
